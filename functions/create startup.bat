5b92f07d8f26c4311eebad82a3e4ae72d50e34a7
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-04-28 15:23:42 +0200

    comments

--- functions/create startup.bat
+++ functions/create startup.bat
@@ -1,1 +1,3 @@
+REM merges all functions into a single file
+REM BUG will probably include itself
 copy /b *.sqf AK_Functions.sqf
--- functions/init.bat
+++ functions/init.bat
@@ -1,2 +1,4 @@
+REM merges the required functions for the Automated Battle Engine into one file
+
 copy /b AK_fnc_spacedvehicles.sqf + AK_fnc_battlelogger.sqf + AK_fnc_AutomatedBattleEngine_Main.sqf init.sqf
-move init.sqf "C:\Google Drive\ArmA 3\Homebrew\Automated Battle Engine"
+REM move init.sqf "C:\Google Drive\ArmA 3\Homebrew\Automated Battle Engine"
