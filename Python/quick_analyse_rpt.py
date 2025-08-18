89076967c4324a2b701f5c4eb716673722ed337b
alphakilo11 <krendl.alexander@gmail.com>
2025-08-18 19:28:00 +0200

    erweitert

--- Python/quick_analyse_rpt.py
+++ Python/quick_analyse_rpt.py
@@ -1,18 +1,19 @@
+# compatible with aKHabD1 and aKHabD2
+from timeit import default_timer as timer
+start_time = timer()
 from datetime import datetime
 import json
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
-import numpy as np
-import pandas as pd
 import re
+print(f"{timer() - start_time} finished imports")
 
 ID_PATTERN = re.compile(r'.*?aKHa(?P<function_handle>[a-zA-Z]{2})(?P<number>\d*)')
 
+print(f"{timer() - start_time} finished static variables")
 
 
-with open(r'C:\Users\krend\AppData\Local\Arma 3\Arma3_x64_2025-08-18_09-47-02.rpt') as file:
+with open(r'C:\Users\krend\AppData\Local\Arma 3\Arma3_x64_2025-08-18_09-47-02.rpt', encoding='utf-8') as file:
     data = file.readlines()
+print(f"{timer() - start_time} finished reading file.")
 cumulated_data = []
 for line in data:
     if re.match(ID_PATTERN, line):
@@ -26,8 +27,34 @@ for line in data:
             log_line = "".join(split_line[1:])
             if len(log_line) > 0 and log_line[0] == "{":
                 try:
-                    cumulated_data.append(json.loads(log_line))
+                    cumulated_data.append((log_timestamp, json.loads(log_line)))
                 except json.JSONDecodeError:
                     print(f'Could not load {"".join(line.split(" ")[2:])}')
-print(len(cumulated_data))
+print(f'{len(cumulated_data)} data lines extracted.' )
+print(f"{timer() - start_time} Done Extracting!")
+# analyze
+shot_count, kill_count = 0, 0
+shooter_unittypes = set()
+MPKilled_events = []
+CriticalDmg_events = []
+for event in cumulated_data:
+    data_dict = event[1]
+    keys = data_dict.keys()
+    if "FirerPosition" in keys:
+        if not "ParentProjectile" in keys: # count shots
+            shot_count += 1
+        shooter_unittypes.add(data_dict["Unittype"])
+    if "Event" in keys:
+        if data_dict["Event"] == "MPKilled":
+            kill_count += 1
+            MPKilled_events.append(event)
+        if data_dict["Event"] not in  ["Deflected", "MPKilled", 'VehicleCriticalDammaged'] and "<NULL-object>" in data_dict.values(): # find incomplete data
+            print(event)
+        if data_dict["Event"] == "Vehicle Critical Dammaged":
+            CriticalDmg_events.append(event)
+print(f"{timer() - start_time} Analysis done!")
+print(f"{shot_count} total shots registered.")
+print(f"{kill_count} total kills registered.")
+print(f"Unique shooting unittypes: {len(shooter_unittypes)}")
+
 #len(re.findall(pattern, data)))
