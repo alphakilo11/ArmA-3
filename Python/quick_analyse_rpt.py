c57f6a0f3dfbd11a31b887a959d0fd85c1982383
alphakilo11 <krendl.alexander@gmail.com>
2025-08-27 19:58:44 +0200

    modified using Flake8

--- Python/quick_analyse_rpt.py
+++ Python/quick_analyse_rpt.py
@@ -3,9 +3,6 @@ import A3_local_utility as arma_local
 import Arma_3_Process_rpt_file as arma
 from timeit import default_timer as timer
 start_time = timer()
-from datetime import datetime
-import json
-import re
 
 arma.print(f"{timer() - start_time} finished imports")
 
@@ -15,7 +12,6 @@ cumulated_data = []
 for missiondata in run_data["Run Missiondata"]:
     cumulated_data.extend(run_data["Run Missiondata"][missiondata]["Extracted Lines"])
 
-#arma.print(f"{timer() - start_time} Done Extracting!")
 # analyze
 shot_count, kill_count = 0, 0
 shooter_unittypes = set()
@@ -24,14 +20,17 @@ CriticalDmg_events = []
 for event in cumulated_data:
     keys = event["data_dict"].keys()
     if "FirerPosition" in keys:
-        if not "ParentProjectile" in keys: # count shots
+        if "ParentProjectile" not in keys:  # count shots
             shot_count += 1
         shooter_unittypes.add(event["data_dict"]["Unittype"])
     if "Event" in keys:
         if event["data_dict"]["Event"] == "MPKilled":
             kill_count += 1
             MPKilled_events.append(event)
-        if event["data_dict"]["Event"] not in  ["Deflected", "MPKilled", 'VehicleCriticalDammaged'] and "<NULL-object>" in event["data_dict"].values(): # find incomplete data
+        if (
+                event["data_dict"]["Event"] not in ["Deflected", "MPKilled", 'VehicleCriticalDammaged']
+                and "<NULL-object>" in event["data_dict"].values()
+        ):  # find incomplete data
             arma.print(event)
         if event["data_dict"]["Event"] == "Vehicle Critical Dammaged":
             CriticalDmg_events.append(event)
