13cb6afb223287b577eeb9889d44c9305588d1ea
alphakilo11 <krendl.alexander@gmail.com>
2025-09-02 00:50:29 +0200

    added projectile_summary()

--- Python/monitor_file.py
+++ Python/monitor_file.py
@@ -3,6 +3,7 @@ import time
 import logging
 import A3_local_utility as arma_local
 import Arma_3_Process_rpt_file as arma
+import numpy as np
 
 LOGFILE_FOLDER = r"C:\Users\krend\AppData\Local\Arma 3"
 FILEPATH = arma_local.find_latest_rpt_file()
@@ -24,6 +25,20 @@ def process_log_line(line: str):
     This function defines how to process each line from the log file.
     You can customize this to send alerts, write to DB, etc.
     """
+    def projectile_summary(projectile_dict):
+        if "Parent Projectile" in projectile_dict[0].keys():
+            return "Submunition not implemented yet"
+        try:
+            shooter_pos = projectile_dict[0]["FirerPosition"]
+        except KeyError:
+            print(f"Error processing {projectile_dict[0]}. Skipping...")
+            return None
+        deleted_pos = projectile_dict[-1]["Position"]
+        total_distance = np.linalg.norm(np.array(shooter_pos) - np.array(deleted_pos))
+        report = f'Time alive: {round(projectile_dict[-1]["tickTime"] - projectile_dict[0]["tickTime"], 2)}'\
+            f'Projectile Travel: {round(total_distance)} m.'
+        return report
+
     global counter
     logging.info(f"New log line: {line.strip()}")
     log_event_dict = arma.extract_marked_lines(line, RUN_METADATA["Current_time"])
@@ -31,12 +46,19 @@ def process_log_line(line: str):
         data_dict = log_event_dict[0]["data_dict"]
         if "FirerPosition" in data_dict.keys():
             for key in ["Unit", "Unittype", "Weapon", "Ammo"]:
-                print(f"{key}: {data_dict[key]}", end=", ")
+                arma.print(f"{key}: {data_dict[key]}", end=", ")
             print()
-            pending_projectiles[data_dict["Projectile"]] = []
+            pending_projectiles[data_dict["Projectile"]] = [data_dict]
             counter += 1
-        if "Event" in data_dict.keys() and data_dict["Event"] == "Deleted":
-            del pending_projectiles[data_dict["Projectile"]]
+        elif "Event" in data_dict.keys():
+            if data_dict["Projectile"] in pending_projectiles.keys():
+                pending_projectiles[data_dict["Projectile"]].append(data_dict)
+            else:
+                pending_projectiles[data_dict["Projectile"]] = [data_dict]
+
+            if data_dict["Event"] == "Deleted":
+                print(projectile_summary(pending_projectiles[data_dict["Projectile"]]))
+                del pending_projectiles[data_dict["Projectile"]]
 
 
 def monitor_log_file():
@@ -55,7 +77,7 @@ def monitor_log_file():
                 if line:
                     process_log_line(line)
                 else:
-                    print(f"{counter} total shots fired. {len(pending_projectiles)} pending projectiles.")
+                    arma.print(f"{counter} total shots fired. {len(pending_projectiles)} pending projectiles.")
                     time.sleep(POLL_INTERVAL)
 
                     # Check for log rotation
